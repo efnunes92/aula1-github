6a3ea6778f7d4fa3a97f465108fcaf0d14495792
Erik Ferreira <efnunes92@gmail.com>
2020-02-29 18:49:49 -0300

    programinha usando enums

---
+++ src/exercicios/Exercicio001.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio001 {
+    public static void main (String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entre com o nome do departamento");
+        String departmentName = sc.nextLine();
+        System.out.print("");
+
+
+
+
+        sc.close();
+    }
+}
