8d32fde4b5e1a406e4865aafa8e709a8308879e0
Erik Ferreira <efnunes92@gmail.com>
2020-02-29 18:05:51 -0300

    programinha usando enums

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,23 @@
 package application;
 
+import entidades.Order;
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Bom dia!");
-		System.out.println("Boa tarde!");
-		System.out.println("Boa noite!");
+
+		Order order = new Order(1080, new Date(), OrderStatus.PAGAMENTO_PENDENTE);
+
+		System.out.println(order);
+
+		OrderStatus os1 = OrderStatus.RECEBIDO;
+		OrderStatus os2 = OrderStatus.valueOf("RECEBIDO");
+
+		System.out.println(os1);
+		System.out.println(os2);
 	}
 
 }
